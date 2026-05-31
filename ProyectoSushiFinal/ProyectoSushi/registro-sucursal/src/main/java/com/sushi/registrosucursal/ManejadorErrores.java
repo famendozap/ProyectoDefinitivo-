facfc33b3e28dfdf429ebe0c7b39b7920b8e9278
Nahemah7 <pe.leivac@duocuc.cl>
2026-05-31 19:35:30 -0400

    clase manjeadorErrores creada en 'registro-sucursal'

--- ProyectoSushiFinal/ProyectoSushi/registro-sucursal/src/main/java/com/sushi/registrosucursal/ManejadorErrores.java
+++ ProyectoSushiFinal/ProyectoSushi/registro-sucursal/src/main/java/com/sushi/registrosucursal/ManejadorErrores.java
@@ -1,4 +1,5 @@
 package com.sushi.registrosucursal;
+
 import com.sushi.registrosucursal.dto.ErrorDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+
 @RestControllerAdvice
 public class ManejadorErrores {
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -46,3 +48,4 @@ public class ManejadorErrores {
         return ResponseEntity.status(500).body(errorDTO);
     }
 }
+
